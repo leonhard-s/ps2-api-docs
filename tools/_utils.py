6885d78aaefb4fda4df8966b0614267036f1bedf
Leonhard S <leonhard-sei@outlook.com>
2021-05-16 18:55:38 +0200

    Add paths factory & template

--- tools/_utils.py
+++ tools/_utils.py
@@ -1,6 +1,9 @@
 """Utility module for repository tools."""
 
+import io
+import re
 import os
+from typing import Any, Dict, List, Tuple
 
 __all__ = [
     'SERVICE_ID'
@@ -9,3 +12,22 @@ __all__ = [
 # The service ID to use for any API interactions. Must be specified for
 # utilities to not get rate limited.
 SERVICE_ID = os.environ.get('SERVICE_ID', 's:example')
+
+# Pattern used to replace expressions in the template YAML
+_PATTERN = re.compile('(\\${{ ?(\\w+?) ?}})')
+
+
+def process_placeholders(template: List[str],
+                         expressions: Dict[str, Any]) -> io.StringIO:
+    buffer = io.StringIO()
+    for line in template:
+        expression: Tuple[str, str]
+        for expression in _PATTERN.findall(line):
+            placeholder, key = expression
+            try:
+                line = line.replace(placeholder, expressions[key])
+            except KeyError:
+                pass
+        buffer.write(line)
+    buffer.seek(0)
+    return buffer
---
+++ tools/generate_paths.py
@@ -0,0 +1,37 @@
+"""A script for generating path items from a template."""
+
+import asyncio
+from typing import TYPE_CHECKING
+
+import auraxium
+
+# Slightly sketchy double-import to appease Pylance
+if TYPE_CHECKING:
+    from ._queries import get_collections
+    from ._utils import SERVICE_ID, process_placeholders
+else:
+    from _queries import get_collections
+    from _utils import SERVICE_ID, process_placeholders
+
+
+async def main(game: str) -> None:
+    """Main script method."""
+    # Get collections
+    async with auraxium.Client(service_id=SERVICE_ID) as client:
+        expressions = [
+            {'datatype': c} for c in await get_collections(game, client)]
+    # Load templates
+    with open('templates/path.yml') as template_file:
+        template = template_file.readlines()
+    # Generate schemata
+    for variables in expressions:
+        datatype = variables['datatype']
+        # "get" query verb
+        with open(f'api/paths/get_{datatype}.yml', 'w') as path_item:
+            path_item.writelines(
+                process_placeholders(template, variables))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main('ps2:v2'))
