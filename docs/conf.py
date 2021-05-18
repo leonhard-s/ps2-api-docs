15ee0ad6dec4015d76a253dddaf327a1ae6d99d0
Leonhard S <leonhard-sei@outlook.com>
2021-05-18 20:21:36 +0200

    Create Sphinx Docs environments
    Docs for the docs to be hosted on "read the docs"... it's docs-ception!

---
+++ docs/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+
+project = 'PS2 API Docs'
+copyright = '2021, Leonhard S.'
+author = 'Leonhard S.'
+version = '0.1'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = []
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The documentation master file
+master_doc = 'index'
+
+# Report broken links and other grievences
+nitpicky = True
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Specify external HTML files to include in the documentation.
+html_additional_pages = {
+    'openapi': 'openapi.html'
+}
---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+.. toctree::
+   :hidden:
+   :maxdepth: 2
+
+=========================================
+Unofficial PlanetSide 2 API Documentation
+=========================================
+
+This project provides detailed, community-maintained documentation for all API collections specific to `PlanetSide 2 <https://www.planetside2.com/home>`_.
+
+The latest version of this documentation is found `here <openapi.html>`_ using the `ReDoc <https://github.com/Redocly/redoc>`_ Documentation viewer.
+
+These docs are maintained in the `OpenAPI 3.0 format <https://swagger.io/specification/>`_ and are designed to be easily consumable by CI/CD toolchains and scripts, do feel free to scrape them for your own API wrappers and utilities.
+
+A download link to the raw OpenAPI JSON specification can be found at the top of the ReDoc version or can be downloaded from the repository's `Releases <https://github.com/leonhard-s/ps2-api-docs/releases>`_ page.
+
+Currently, only the REST endpoint is documented. A WebSocket API reference may be added in a later version.
+
+For more information, visit the project repository at https://github.com/leonhard-s/ps2-api-docs.
