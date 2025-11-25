ac43038e2a7cc6fa9ad28ac1ae661adb83803728
Leonhard S. <leonhard-sei@outlook.com>
2025-11-25 16:32:41 +0300

    Fix unused RTD dir

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Specify external HTML files to include in the documentation.
 html_additional_pages = {
